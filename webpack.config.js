38d99dddfffbce8b5dd45ee2398e3e1d319486cf
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2021-09-20 15:31:13 +0300

    added babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Langton\'s Ant',
+      template: 'index.html',
+    }),
+  ],
+};
